832141379d0bcc07f6ff3179966ee93c962e313b
janosvajda <vajdajanos@gmail.com>
2022-07-10 02:54:53 +0100

    Login screen input fields have been created.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image,  TextInput,
+  Button,
+  TouchableOpacity, } from 'react-native';
+import React, { useState } from 'react';
 
 export default function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <View style={styles.container}>
-      <Text>!HƎlp</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={{ uri: 'https://github.com/janosvajda/help-mobile-app/blob/main/assets/icon.png' }}
+        style={{ width: 40, height: 40 }}
+      />
+       <View style={styles.inputView}>
+    <TextInput
+      style={styles.TextInput}
+      placeholder="Email."
+      placeholderTextColor="#003f5c"
+      onChangeText={(email) => setEmail(email)}
+    />
+  </View>
+   
+  <View style={styles.inputView}>
+    <TextInput
+      style={styles.TextInput}
+      placeholder="Password."
+      placeholderTextColor="#003f5c"
+      secureTextEntry={true}
+      onChangeText={(password) => setPassword(password)}
+    />
+  </View>
     </View>
   );
 }
@@ -16,5 +42,42 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  image :{
+    marginBottom: 40,
+    height: 100,
+    width: 100
+  },
+  
+  inputView: {
+    backgroundColor: "#FFC0CB",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+ 
+    alignItems: "center",
+  },
+ 
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+ 
+  forgot_button: {
+    height: 30,
+    marginBottom: 30,
+  },
+ 
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
   },
 });
