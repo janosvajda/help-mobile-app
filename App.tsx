727c5abedb8b6631d51193b15a1d5bd026bc2fb6
janosvajda <vajdajanos@gmail.com>
2022-07-10 10:17:45 +0100

    AsyncStorage save & load functions has been implemented.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
 import {
   StyleSheet, Text, View, Image, TextInput,
   TouchableOpacity
---
+++ src/components/Storage.tsx
@@ -0,0 +1,46 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeSingleValue = async (value) => {
+    try {
+        await AsyncStorage.setItem('@storage_Key', value)
+    } catch (e) {
+        const error = {
+            'message': 'Error: storeSingleValue',
+            'value': value,
+            'error': e?.message
+        }
+        console.warn(error);
+        throw new Error(error.message);
+    }
+}
+
+const storeObject = async (value) => {
+    try {
+        const jsonValue = JSON.stringify(value)
+        await AsyncStorage.setItem('@storage_Key', jsonValue)
+    } catch (e) {
+        const error = {
+            'message': 'Error: storeObject',
+            'value': value,
+            'error': e?.message
+        }
+        console.warn(error);
+        throw new Error(error.message);
+    }
+}
+
+export const getData = async () => {
+    try {
+        const value = await AsyncStorage.getItem('@storage_Key')
+        if (value !== null) {
+
+        }
+    } catch (e) {
+        const error = {
+            'message': 'Error: getData',
+            'error': e?.message
+        }
+        console.warn(error);
+        throw new Error(error.message);
+    }
+}
